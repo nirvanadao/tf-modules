43dfa2a811130a613210c0be4c02e69ca4db7e72
nirvana-sid <sid@nirvana.finance>
2025-12-30 21:18:09 -0600

    limit iam for sig streamer

--- modules/stream-sigs/iam.tf
+++ modules/stream-sigs/iam.tf
@@ -7,8 +7,9 @@ resource "google_pubsub_topic_iam_member" "worker_publisher" {
 }
 
 # Grant worker service account access to GCS checkpoint bucket
+# Using objectUser (read/write) instead of objectAdmin (no delete/admin needed)
 resource "google_storage_bucket_iam_member" "worker_checkpoint_access" {
   bucket = google_storage_bucket.checkpoint_bucket.name
-  role   = "roles/storage.objectAdmin"
+  role   = "roles/storage.objectUser"
   member = "serviceAccount:${module.worker_service.service_account_email}"
 }

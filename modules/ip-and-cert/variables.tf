4cdd40c71d132560b879f6a2f3c0c3a087eca88e
nirvana-sid <sid@nirvana.finance>
2026-01-11 19:51:51 -0600

    existing ip and cert

--- modules/ip-and-cert/variables.tf
+++ modules/ip-and-cert/variables.tf
@@ -17,3 +17,15 @@ variable "domain" {
   description = "Domain for SSL certificate"
   type        = string
 }
+
+variable "existing_ip_address" {
+  description = "Existing static IP address. If provided, skip IP creation."
+  type        = string
+  default     = null
+}
+
+variable "existing_ssl_certificate_id" {
+  description = "Existing SSL certificate ID. If provided, skip certificate creation."
+  type        = string
+  default     = null
+}
